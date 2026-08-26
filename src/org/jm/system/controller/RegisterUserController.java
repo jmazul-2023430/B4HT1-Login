56da88bba96153ddb50e87d80688589b316058af
Jefferson Mazul <jmazul-2023430@kinal.edu.gt>
2026-08-26 09:50:34 -0600

    feat: Validacion en campo de registro

--- src/org/jm/system/controller/RegisterUserController.java
+++ src/org/jm/system/controller/RegisterUserController.java
@@ -8,7 +8,11 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import org.jm.system.utils.AlertInformation;
+import org.jm.system.utils.Validations;
 import org.jm.system.utils.ViewFactory;
 
 /**
@@ -16,6 +20,22 @@ import org.jm.system.utils.ViewFactory;
  * @author informatica
  */
 public class RegisterUserController implements Initializable {
+
+    @FXML
+    private TextField txtUser;
+    @FXML
+    private TextField txtName;
+    @FXML
+    private TextField txtLastName;
+    @FXML
+    private TextField txtEmail;
+    @FXML
+    private PasswordField pwdPassword;
+    @FXML
+    private PasswordField pwdConfirmPassword;
+
+    private Validations validate = new Validations();
+    private AlertInformation alertInfo = new AlertInformation();
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -27,9 +47,41 @@ public class RegisterUserController implements Initializable {
         ViewFactory viewFacto = new ViewFactory();
         viewFacto.viewLogin();
     }
-    
+
+    //Validar el error del correo
     @FXML
-    public void onCreateUser(MouseEvent event){
+    public void onCreateUser(MouseEvent event) {
+        boolean isValidEmail = validate.validateEmail(txtEmail.getText().trim());
+        if (isValidEmail == false) {
+            alertInfo.viewAlert(3, "ERROR EMAIL", "ERROR DE CAMPO", "HAS INGRESADO UN EMAIL INCORRECTO");
+        }
+
+        String user, name, lastName, email, password, confirmPassword;
+        user = txtUser.getText().trim();
+        name = txtName.getText().trim();
+        lastName = txtLastName.getText().trim();
+        email = txtEmail.getText().trim();
+        password = pwdPassword.getText().trim();
+        confirmPassword = pwdConfirmPassword.getText().trim();
+
+        if (validate.emptyText(user) == true
+                || validate.emptyText(name) == true
+                || validate.emptyText(lastName) == true
+                || validate.emptyText(email) == true
+                || validate.emptyText(confirmPassword) == true
+                || validate.emptyText(confirmPassword) == true) {
+
+            alertInfo.viewAlert(3, "ERROR DE CAMNPOS VACIOS", "ERROR DE CAMPO", "DEJO CAMPOS VACIOS EN EL FORMULARIO");
+            return;
+        }
+        
+        if(validate.validateLengthText(user, 25) || 
+                validate.validateLengthText(name, 50) ||
+                validate.validateLengthText(lastName, 50) ||
+                validate.validateLengthText(password, 50) ||
+                validate.validateLengthText(confirmPassword, 50)){
+            
+        }
         
     }
 
--- src/org/jm/system/utils/Validations.java
+++ src/org/jm/system/utils/Validations.java
@@ -9,26 +9,53 @@ package org.jm.system.utils;
  * @author informatica
  */
 public class Validations {
-    
-    public Validations(){}
-    public Boolean equalsText(String textOriginal, String textCompare){       
-       return textOriginal.equals(textCompare);
+
+    public Validations() {
     }
-    
-    public Boolean emptyText(String text){
+
+    public Boolean equalsText(String textOriginal, String textCompare) {
+        return textOriginal.equals(textCompare);
+    }
+
+    public Boolean emptyText(String text) {
         boolean isEmpty = false;
-        
-        if (text.isEmpty() || text.isBlank())
+
+        if (text.isEmpty() || text.isBlank()) {
             isEmpty = true;
-        
+        }
+
         return isEmpty;
     }
-    
-    public Boolean validateLengthText(String text, int lengthMax){
-            return text.length()<= lengthMax;
+
+    public Boolean validateLengthText(String text, int lengthMax) {
+        return text.length() <= lengthMax;
     }
-    
-    public Boolean validationEmail(String email){
+
+    public Boolean validateEmail(String email) {
+        int dotCount = 0;
+        int arrobeCount = 0;
+
+        for (int index = 0; index < email.length(); index++) {
+            if (email.charAt(index) == '.') {
+                dotCount++;
+            }
+
+            if (dotCount > 1) {
+                return false;
+            }
+        }
+
+        for (int index = 0; index < email.length(); index++) {
+            if (email.charAt(index) == '@') {
+                arrobeCount++;
+            }
+        }
+
+        if (arrobeCount != 1) {
+            return false;
+        }
+
         return true;
+
     }
 }
